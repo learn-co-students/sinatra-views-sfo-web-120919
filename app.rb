bc68954992056bc484587a3b0dc8341db523ffbd
brookebachman00 <yourpetlover4411@yahoo.com>
2020-01-06 15:46:46 -0800

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		
-	end
+	# get '/' do
+	# 	"Hello World"
+	# end
+	# get '/' do
+	# 	"This HTML code is inside of a '.erb' file"
+	# end
+
+	# get "/info" do
+	# 	"Testing the info page"
+	#   end
+
+	  get '/' do
+		erb :index
+	  end
+   
+	  get "/info" do
+		erb :info
+	  end
+
+
 end
